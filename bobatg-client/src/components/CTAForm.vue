8e4cbba971cc8481e15c8eaa0139c54494167515
Jesse Ma <j.masonoma17@gmail.com>
2020-04-24 14:54:13 -0700

    Add ctaform component to Home page

---
+++ bobatg-client/src/components/CTAForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="ctaform m-5">
+      <b-form inline @submit="onSubmit" @reset="onReset">
+        <b-form-group 
+          label-for="teashopname-input"
+        >
+          <b-form-input
+            id="teashopname-input"
+            v-model="teashopname"
+            type="text"
+            required
+            placeholder="Tea Shop Name"
+          ></b-form-input>
+        </b-form-group>
+        <b-button type="submit" variant="primary">Submit</b-button>
+      </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- bobatg-client/src/views/Home.vue
+++ bobatg-client/src/views/Home.vue
@@ -1,17 +1,29 @@
 <template>
-  <div class="demo">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="home m-5">
+    <b-container>
+      <b-row class="text-center">
+        <b-col>
+          <h1>Cupidatat adipisicing commodo tempor et duis ex in</h1>
+          <h2>Ea fugiat eiusmod sunt tempor fugiat aliquip velit aliqua qui velit anim</h2>
+        </b-col>
+      </b-row>
+      <b-row align-h="center">
+        <b-col>
+          <CTAForm />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import CTAForm from '@/components/CTAForm.vue';
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    CTAForm
   }
 };
 </script>
