dfe356059d04b7f6581a3977da74107b2220c306
Jesse Ma <j.masonoma17@gmail.com>
2020-04-27 21:29:22 -0700

    Add footer section

---
+++ bobatg-client/src/components/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="footer mt-5">
+    <b-container>
+      <b-row class="py-4">
+        <b-col>
+          <h3 class="mt-3">In esse quis nulla</h3>
+          <p>Adipisicing labore</p>
+          <p>Ea proident veniam</p>
+          <p>Ea adipisicing enim</p>
+        </b-col>
+        <b-col>
+          <h3 class="mt-3">In esse quis nulla</h3>
+          <p>Adipisicing labore</p>
+          <p>Ea proident veniam</p>
+          <p>Ea adipisicing enim</p>
+        </b-col>
+        <b-col>
+          <h3 class="mt-3">In esse quis nulla</h3>
+          <p>Adipisicing labore</p>
+          <p>Ea proident veniam</p>
+          <p>Ea adipisicing enim</p>
+        </b-col>
+        <b-col>
+          <h3 class="mt-3">In esse quis nulla</h3>
+          <p>Adipisicing labore</p>
+          <p>Ea proident veniam</p>
+          <p>Ea adipisicing enim</p>
+        </b-col>            
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style>
+
+</style>
