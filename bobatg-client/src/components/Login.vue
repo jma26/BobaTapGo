7eb1bb980b09d740e09e3ef41294c02e67f044d2
Jesse Ma <j.masonoma17@gmail.com>
2021-01-26 23:15:26 -0800

    Update login modal with form inputs, simple falsy validation

--- bobatg-client/src/components/Login.vue
+++ bobatg-client/src/components/Login.vue
@@ -1,12 +1,80 @@
 <template>
   <div class="login">
-    <b-modal id="loginModal">This is the Login Modal</b-modal>
+    <b-modal
+      id="loginModal"
+      title="Login"
+      hide-footer="true"
+      @ok="handleOk"
+    >
+      <form ref="form" @submit.stop.prevent="handleSubmit">
+        <!-- Email Group -->
+        <b-form-group
+          label="Email"
+          label-for="email-input"
+          invalid-feedback="Email is required"
+        >
+          <b-form-input
+            id="email-input"
+            placeholder="youremail@example.com"
+            :state="emailState"
+            v-model="email"
+            required
+          >
+          </b-form-input>
+        </b-form-group>
+        <!-- Password Group -->
+        <b-form-group
+          label="Password"
+          label-for="password-input"
+          invalid-feedback="Password is required"
+        >
+          <b-form-input
+            id="password-input"
+            placeholder="*******"
+            :state="passwordState"
+            v-model="password"
+            required
+          >
+          </b-form-input>
+        </b-form-group>
+        <b-button block type="submit" variant="primary">Login</b-button>
+      </form>
+    </b-modal>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Login"
+  name: "Login",
+  data() {
+    return {
+      email: '',
+      password: '',
+      emailState: null,
+      passwordState: null,
+    }
+  },
+  methods: {
+    checkFormValidity: function() {
+      const valid = this.$refs.form.checkValidity()
+      this.emailState = this.passwordState = valid;
+      return valid;
+    },
+    handleOk: function(modalEvent) {
+      modalEvent.preventDefault();
+      this.handleSubmit();
+    },
+    handleSubmit: function() {
+      // Exit when form isn't valid
+      if (!this.checkFormValidity()) {
+        return
+      }
+      // Close modal manually on success
+      this.$nextTick(() => {
+        this.$bvModal.hide('loginModal')
+      })
+    }
+  }
 }
 </script>
 
