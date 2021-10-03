0aafb3fcea3e0eaf1b935eaa1faef9c9d6664895
Jesse Ma <j.masonoma17@gmail.com>
2021-10-02 17:09:11 -0700

    Fix typo on Store controller

--- server/README.md
+++ server/README.md
@@ -4,3 +4,8 @@ Backend application for BobaTapGo
 ## Concept
 * Store Schema
 * User Schema
+
+## Technologies
+* React
+* MongoDB
+* Node.js v14.16.0
--- server/controllers/store.controller.js
+++ server/controllers/store.controller.js
@@ -4,7 +4,7 @@ exports.createUser = async (req, res) => {
   let newStore = { ...req.body }
   
   try {
-    const store = await new store(newStore).save();
+    const store = await new Store(newStore).save();
     res.status(201).send(store);
   } catch (err) {
     res.status(400).send(err.message);
